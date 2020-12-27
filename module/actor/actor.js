4279e127bdb849676d404be3c72448b021cba89c
ricardoruizfp <rfpimpao@gmail.com>
2020-12-27 23:10:28 +0000

    commented out code for reference

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -3,7 +3,6 @@
  * @extends {Actor}
  */
 export class MTA20AActor extends Actor {
-
   /**
    * Augment the basic actor data with additional dynamic data.
    */
@@ -16,7 +15,7 @@ export class MTA20AActor extends Actor {
 
     // Make separate methods for each Actor type (character, npc, etc.) to keep
     // things organized.
-    if (actorData.type === 'character') this._prepareCharacterData(actorData);
+    if (actorData.type === "character") this._prepareCharacterData(actorData);
   }
 
   /**
@@ -28,10 +27,9 @@ export class MTA20AActor extends Actor {
     // Make modifications to data here. For example:
 
     // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
+    // for (let [key, ability] of Object.entries(data.abilities)) {
+    // Calculate the modifier using d20 rules.
+    // ability.mod = Math.floor((ability.value - 10) / 2);
+    // }
   }
-
 }
